c38b1a3f7a155f06761c383b7cb62a3a56d6c44d
NgozikaNwachukwu <139152775+NgozikaNwachukwu@users.noreply.github.com>
2024-09-15 20:43:53 -0700

    Add files via upload

---
+++ Autobio/index.php
@@ -0,0 +1,174 @@
+<?php
+include"C:\wamp64\www\System\config.php";
+
+if($maintenance == true){
+	echo "The site is currectly under maintenance";
+}elseif($maintenance == false){
+	?>
+<html>
+	<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="/frontend/design/css/stylesheet.css">
+		<title><?php echo $title; echo $separator; echo $description;?> Stylish Autobiography</title>
+		<script src="https://kit.fontawesome.com/b78fdb3e70.js" crossorigin="anonymous"></script>
+		<meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
+        
+        <link href="C:\wamp64\www\frontend\design\css\bootstrap.min.css" rel="stylesheet" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link
+href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+
+	</head>
+<body>
+	<div id="wrapper">
+		<div>
+			A SHORT AUTOBIOGRAPHY
+		</div>
+        <div>
+	        OF
+        </div>
+        <div>
+	        MISS NGOZIKA NWACHUKWU
+        </div>
+    </div>
+    <img src="me.jpg" alt="Stylish Autobiography" width="130" height="180" border="6" align="right">
+
+    <div id= "Birth">
+    	<h1>Birth and Family</h1>
+    	
+<div>
+        My Name is Ngozika Blessing Nwachukwu and im 18 years old. I was Born on the 7th of Decebember, 2005 in the city of Aba, Abia, Nigeria.
+    </div>
+    <div>
+        I curently reside in Toronto and I go to Ontario tech university and am currently in secnd year. I live with my 3 sisters; Laura, Karen and Kim.
+    </div>
+    <div>
+        I have a fourth sister, Sharon and a brother, David. My parents and brother still live back home and i miss them everyday.	
+    </div>
+    <div>
+        I am the 5th out of 6 children and I grew up in a very loving home and I cherish them deeply.
+    </div>
+     
+     <div id= "Birth">
+    	<h1>Childhood</h1>
+    	
+    <div>
+        I started school around when i was 2 years old. I wnt to pre-kindergarten in aba, Then when we moved to Abuja I went to Kindergarten and primary in Living fountain International.    
+    </div>
+    <div>
+       While i was there, I mostly won awrds for best in mathematics and science. Throughout my Kindergarten and Primary Those were the subjects I was Naturally good at and had a genuine interest for.  
+    </div>
+    <div>
+    	Looking at how beautiful my older sisters were I never thought I was going to be that pretty so I never really worried nor cared about my Appearance, Grades were always where i shined the most at anyway. 
+    </div>
+    <div>
+        The first friend I ever made was Sharon. We were in the same Kindergarten class. One day whilst in the english lesson, she kept asking the techer to go to the bathroom but she didnt let her.
+    </div>
+    <div>
+        She enede up peeing on the floor, the teacher screamed at her to clean it up and everyone laughed at her but me. She sterted to cry and I felt like crying when I saw her cry. That is when i realized how empathetic I was. I felt like everyone was laughing at me and hated that.
+    </div>
+    <div>
+    	After she cleened it up, I went to give her a napkin from my lunch bag to clean her tears, Then we became best friends. I was so excited to have my first friend that I got distracted from school and got my first B. I genuinely thought it was the end of the world and cried about it but I learnt how to baalnce having a friend and being a good student.
+    	<div>
+    	I really wish I knew where Sharon was today.
+    	Before I left the school I made a whole bunch of new friends as well.
+    	</div>
+
+        <div id="About">
+    		<h1> About Me</h1>
+    		<div>
+    	My favorite colour is Green, and I love dogs. I had a dog named Dexter, I loved that dog more than anything. My dad got him for me when I was 7 and he saidly died of cancer back in 2020 and I miss him everyday. I love fied eggs. I could Live on them alone if I wasnt going to be judged.	
+    		</div>
+    		<div>
+    	I love swimming and playing badminton and have gold medals from both sports. I would say I am a bit of a perfectionist, because I believe I must be the absolute best in everything I do and "anything worth doing must be done very well" as my Mom always says.
+    		</div>
+    		<div>
+    	I am very empathetic naturally. Seeing an old person sitting alone can make me break down and im that one person that cries evertime they watch finding nemo. I alos hate horror movies, I genuinely get scared. 
+    		</div>
+    		<div>
+    			Overall I am a very loyal and loving person also very dedicated and driven.
+    		</div>
+
+
+    	<div id="Young Adolescence">
+    		<h1> Young Adolescence</h1>
+    		<div>
+    	I left primary school after 5th grade and went to secondary school. The name of The secondary institution I attended was Regina Pacis and this was a very pivotal momemnt of my life.		
+    		</div>
+    		<div>
+    	This was around the time i decided who I wanted to be in life and part of that reason was because of my father. I was old enough to realize how privileged I am, and how blessed I was to have a comfortable life.		
+    		</div>
+    		<div>
+    	Thats when I knew I wanted to be an engineer- just like my Father is. It was an amazing idea and i was already good in sciences so I knew it will be a good path to follow so i can be able to give my present and future family a comfortable life just like my dad did.
+    		</div>
+    		<div>
+    	I initially decided to do aerospace engineering and become an astronaut, but when I got to highschool I chickened out when I found out you can get stuck in space, and what is one exactly supposed to do in a situation like that?
+    		</div>
+    		<div>
+    	I then decided to persue software engineering because I thought and still think its really cool although I wish it was easier because it gets really stressful. But if it were easy, everyone would do it.
+    		</div>
+            <div>
+        Anyway, While in this school -which for context was an all girl's boarding school- ironically is what drove my passion for intersectional feminism. Due to a lot of bad experiences I had there, made me want to be a women's rights activist/overall human's rights activist as well in the future, but i want to pursue engineering first.
+            </div>
+          <div>
+        It was not all bad though, I met my current best friend there and made a lot of good friends and meomries on the way, Although I genuinely do not recommend it for anyone.
+          </div>
+
+<div id= "Life">
+<h1>What Life Means To Me</h1>
+
+<div>
+	Life to me is surrounding yourself with the right people -that is people you trust with your life. That might seem too much for some people but i would never surround myself with anyone I do not trust or Love.
+</div>
+<div>
+	Life for me is also being authentically yourself and doing/being/surrounding yourself with what and who makes you happy. At the end of the day we can die at any time, of literaaly any cause, so its better taht you try your best to live your life to the fullest. Although I know it is hard. I always think, if I am in a situation that im about to die can i say "I lived a happy and fullfilling life?"
+</div>
+<div>
+	Life for me is also fighting for what you believe in till the end. Even if anyone makes me feel like it doesnt matter or it shouldnt matter that will never stop me for fighting for what I believe is right and fighting for the ones I love.
+	And overall Life is about upholding what you belive/your standards and never letting anyone or anything come between that.
+</div>
+
+<div id= "Overall">
+<h1>Overall</h1>
+
+<div>
+    To conclude, I am young and still have a very long way to go in life. Though, I feel like I might know it all, I know I still have a lot to learn in basically every sector of life. Although I am thankful for all the experiences that I have had in life because they have shaped me into who I am today, and I hope that I will continute to go through life being unapologeticaly me. 
+</div>
+<div>
+</div>
+<div>Thank you so much for reading!</div>
+
+<div id= "footer" style="background-color: #dcdad7; width:1265px; text-align: center;">
+
+	<h1>Contact Me:</h1>
+	<a href="https://www.linkedin.com/in/ngozika-nwachukwu-75a104329/?trk=opento_sprofile_topcard" target="_blank">LinkedIn</a>
+</div>
+
+
+
+</div>
+</div>
+
+
+
+    	</div>
+
+    </div>
+    </div>
+</body>
+</html>
+	<?php
+}
+
+?>
---
+++ System/config.php
@@ -0,0 +1,12 @@
+<?php
+$title=      "My Autobiography";
+$separator = " - ";
+$description="Good way to show how to use PHP,HTML and CSS to create a fun autobiography on the web!";
+
+$maintenance=false;
+
+$dbserver=   "";
+$dbusername ="";
+$dbpassword= "";
+$db =        "";
+?>
